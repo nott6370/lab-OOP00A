c1e3a8e099f0292a1b6274a8a9c1b18c73e45bcd
jaruvit <jaruvit.s@kkumail.com>
2022-11-28 15:32:54 +0700

    Add name id email

---
+++ LABOOP00A/LABOOP00A/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//print string to command line
+Console.WriteLine("Name: Jaruvit Sangkaewsirikul");
+Console.WriteLine("ID: 653450086-7");
+Console.WriteLine("Email: jaruvit.s@kkumail.com");
