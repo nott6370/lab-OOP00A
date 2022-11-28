e2ab81562242ce1ea66ac8393a5fea9ed03e2cb8
jaruvit <jaruvit.s@kkumail.com>
2022-11-28 15:35:09 +0700

    Update Program.cs

--- LABOOP00A/LABOOP00A/Program.cs
+++ LABOOP00A/LABOOP00A/Program.cs
@@ -1,6 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 //print string to command line
+
+//Name
 Console.WriteLine("Name: Jaruvit Sangkaewsirikul");
+
+//ID
 Console.WriteLine("ID: 653450086-7");
+
+//Email
 Console.WriteLine("Email: jaruvit.s@kkumail.com");
